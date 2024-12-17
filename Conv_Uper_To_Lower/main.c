5234f38d94efa173ee24e67c1532908c44cec4a7
helvl12 <helal.study.00@gmail.com>
2024-12-17 09:40:51 +0200

    Edit

--- Conv_Uper_To_Lower/main.c
+++ Conv_Uper_To_Lower/main.c
@@ -4,14 +4,18 @@ int main() {
     char ch;
     printf("Enter a character: ");
     scanf(" %c", &ch); 
+	
     if (ch >= 'a' && ch <= 'z') {
         ch = ch - 32;
+		printf("Converted character: %c\n", ch);
     }
     else if (ch >= 'A' && ch <= 'Z') {
         ch = ch + 32;
-    }
-
-    printf("Converted character: %c\n", ch);
-
+		printf("Converted character: %c\n", ch);
+    }	
+	else if (ch >= '0' && ch <= '9') {
+		printf("That is not a letter");
+	}
+	
     return 0;
 }
